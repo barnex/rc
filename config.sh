fa86b34c5835b16961700e039c8b4a53b61fe2ea
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2017-08-11 09:52:03 +0200

    tweak nautilus

--- config.sh
+++ config.sh
@@ -10,6 +10,8 @@ ln -s -v -f $RC/config $HOME/.config/i3/config
 ln -s -v -f $RC/roxterm.sourceforge.net/ $HOME/.config
 
 gsettings set org.gnome.desktop.background show-desktop-icons false
+gsettings set org.gnome.nautilus.window-state start-with-sidebar false
+gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 
 mkdir $HOME/bin
 mkdir $HOME/src
